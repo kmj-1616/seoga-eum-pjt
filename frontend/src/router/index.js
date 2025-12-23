2726921b43a844344d1e05608c898d10039324d9
kmj112503 <kmj112503@gmail.com>
2025-12-23 15:13:10 +0900

    fix: 검색 목록 100권 단위 페이지 추가, 홈 검색 아이콘 수정, 비로그인 시 프로필 이동 막음

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -45,8 +45,13 @@ router.beforeEach((to, from, next) => {
   // 1. 이미 로그인했는데 로그인(/login)이나 회원가입(/signup) 페이지로 가려는 경우
   if (isLoggedIn && (to.name === 'login' || to.name === 'signup')) {
     alert('이미 로그인된 상태입니다.')
-    next({ name: 'home' }) // 홈으로 튕겨냄
+    next({ name: 'home' }) 
   } 
+  // 2. 로그인을 안 했는데 '내 서재(profile)' 페이지로 가려는 경우 
+  else if (!isLoggedIn && to.name === 'profile') {
+    alert('신분 확인이 필요합니다. 로그인 페이지로 이동합니다.')
+    next({ name: 'login' })
+  }
   else {
     next() // 그 외의 경우는 정상 이동
   }
